5105461eb49f420b27b864cdccb404d2fbf56553
minjee kim <hbmj1017@naver.com>
2025-02-03 20:10:31 +0900

    Feat(env): set tailwind, scss, postcss
    - 설치 : tailwindcss, postcss, autoprefixer, sass path
    - 설정 파일 : 설치 후 설정 파일 및 tsconfig.*.json 파일 내 alias 설정 진행
    - tsconfig.json 형태에서 프로젝트 레퍼런스 형태 유지
    
    JIRA: S12P11A202-39

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // Node.js 내장 모듈, 파일 경로 조작 유틸 제공
+
+// __dirname 설정 (ESM 환경에서 필요)
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); // 현재 실행 중인 파일 디렉토리 경로
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/styles/variables.scss";',
+      },
+    },
+  },
 });
