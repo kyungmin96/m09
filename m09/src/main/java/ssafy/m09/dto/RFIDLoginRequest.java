29e6087becbcfe24e297d3d1756ff77a9231a45d
byungji <qudwl33@naver.com>
2025-02-04 13:07:57 +0900

    Feat: add authorities option, login, logout
    - JWT 토큰 발급 기능 추가
    - 로그인, 로그아웃 기능 추가
    
    JIRA: S12P11A202-62

--- m09/src/main/java/ssafy/m09/dto/RFIDLoginRequest.java
+++ m09/src/main/java/ssafy/m09/dto/RFIDLoginRequest.java
@@ -1,12 +1,26 @@
 package ssafy.m09.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class RFIDLoginRequest {
-    @JsonProperty("UID")
+
     private String uid;
+
+    // 기본 생성자 (필수)
+    public RFIDLoginRequest() {}
+
+    // JSON 매핑을 위한 생성자
+    @JsonCreator
+    public RFIDLoginRequest(@JsonProperty("UID") String uid) {
+        this.uid = uid;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
 }
