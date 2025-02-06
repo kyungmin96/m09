e306a2f34c0718ad6f98878ae4c42c0c94f8d7d2
rokja97 <rokja97@gmail.com>
2025-02-06 10:50:47 +0900

    Feat: streaming module
    - 영상 스트리밍 모듈 구현현

---
+++ m09/src/embedded/m09_stream_uploader.py
@@ -0,0 +1,23 @@
+import cv2
+import socketio
+import os
+
+port = 8765
+__address = os.environ["STREAM_SERVER_ADDRESS"] + f":{port}"
+sio_cli = socketio.Client()
+sio_cli.connect(__address)
+
+# cv 이미지 프레임을 지정된 웹소켓에 스트리밍
+def stream_cv_frame(frame):
+    bytecode = __bytecode_cv_frame(frame)
+    sio_cli.emit("stream", bytecode)
+
+# 바이트 코드로 변환
+def __bytecode_cv_frame(frame):
+    width = 640
+    height = 480
+    return cv2.imencode('.jpg', frame)[1].tobytes()
+
+@sio_cli.on("response")
+def response(data):
+    print(data)  # {'from': 'server'}
