68844a39df2d183acfc0363d4de09d042e7fde58
byungji <qudwl33@naver.com>
2025-02-07 14:47:08 +0900

    Feat: add updatedat
    - 모든 엔티티에 onupdate추가
    
    JIRA: S12P11A202-62

--- m09/src/main/java/ssafy/m09/service/TaskService.java
+++ m09/src/main/java/ssafy/m09/service/TaskService.java
@@ -2,13 +2,11 @@ package ssafy.m09.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 import ssafy.m09.domain.Task;
 import ssafy.m09.dto.TaskRequest;
 import ssafy.m09.repository.TaskRepository;
 
-import java.nio.file.OpenOption;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -20,18 +18,19 @@ public class TaskService {
 
     @Transactional
     public Task createTask(TaskRequest request) {
+        //
         Task task = Task.builder()
                 .title(request.getTitle())
                 .content(request.getContent())
-                .comment(request.getComment())
+//                .comment(request.getComment())
                 .location(request.getLocation())
-                .assignedUser(request.getAssignedUser())
-                .vehicle(request.getVehicle())
-                .scheduledStartTime(request.getScheduledStartTime())
-                .scheduledEndTime(request.getScheduledEndTime())
-                .startTime(request.getStartTime())
-                .endTime(request.getEndTime())
-                .taskState(request.getTaskState())
+//                .assignedUser(request.getAssignedUser())
+//                .vehicle(request.getVehicle())
+//                .scheduledStartTime(request.getScheduledStartTime())
+//                .scheduledEndTime(request.getScheduledEndTime())
+//                .startTime(request.getStartTime())
+//                .endTime(request.getEndTime())
+//                .taskState(request.getTaskState())
                 .updatedAt(LocalDateTime.now())
                 .build();
 
