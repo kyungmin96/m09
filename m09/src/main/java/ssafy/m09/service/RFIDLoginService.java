f953f780a45a15a3c45baafd5d97bfbb17d6830a
byungjikim <qudwl72@gmail.com>
2025-02-09 13:01:56 +0900

    Feat: refactor RFID
    - RFID 기능 리팩토링 및 테스트 완료
    - RFID Controller에 토큰 유무 추후 추가 해야됨
    - Test 로직 안쓰고 postman으로 테스트할거라 삭제함
    
    JIRA: S12P11A202-62

--- m09/src/main/java/ssafy/m09/service/RFIDLoginService.java
+++ m09/src/main/java/ssafy/m09/service/RFIDLoginService.java
@@ -1,9 +1,13 @@
 package ssafy.m09.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import ssafy.m09.domain.RFID;
 import ssafy.m09.domain.User;
+import ssafy.m09.dto.common.ApiResponse;
+import ssafy.m09.dto.request.RFIDLoginRequest;
+import ssafy.m09.global.error.ErrorCode;
 import ssafy.m09.repository.RFIDRepository;
 
 import java.util.Optional;
@@ -13,9 +17,21 @@ import java.util.Optional;
 public class RFIDLoginService {
     private final RFIDRepository rfidRepository;
 
-    public Optional<User> loginWithRFID(String cardKey)
+    public ApiResponse<User> loginWithRFID(RFIDLoginRequest request)
     {
-        Optional<RFID> rfid = rfidRepository.findByCardKey(cardKey);
-        return rfid.map(RFID::getUser);
+        String cardKey = request.getUid();
+
+        if (cardKey == null || cardKey.isEmpty()) {
+            return ApiResponse.error(HttpStatus.BAD_REQUEST, "카드 키가 비어 있습니다.");
+        }
+
+        Optional<RFID> rfidOptional = rfidRepository.findByCardKey(cardKey);
+
+        if (rfidOptional.isEmpty()) {
+            return ApiResponse.error(HttpStatus.NOT_FOUND, ErrorCode.RESOURCE_NOT_FOUND.getMessage(), "RFID_NOT_FOUND");
+        }
+
+        User user = rfidOptional.get().getUser();
+        return ApiResponse.success(user, "RFID 로그인 성공");
     }
 }
