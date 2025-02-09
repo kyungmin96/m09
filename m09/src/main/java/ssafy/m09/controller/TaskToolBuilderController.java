e54eed57e52388138b4cb4286e2c882269b6c08f
byungjikim <qudwl72@gmail.com>
2025-02-09 15:10:15 +0900

    Feat: refactor TaskToolBuilder
    - 기능 리팩토링 및 테스트 완료
    - 토큰 헤더 추가 요망
    
    JIRA: S12P11A202-62

--- m09/src/main/java/ssafy/m09/controller/TaskToolBuilderController.java
+++ m09/src/main/java/ssafy/m09/controller/TaskToolBuilderController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ssafy.m09.domain.TaskToolBuilder;
+import ssafy.m09.dto.common.ApiResponse;
 import ssafy.m09.dto.request.TaskRequest;
 import ssafy.m09.service.TaskToolBuilderService;
 
@@ -16,42 +17,28 @@ import java.util.Optional;
 public class TaskToolBuilderController {
     private final TaskToolBuilderService taskToolBuilderService;
 
-    // RESTful 한게 어떻게 에러 메시지를 보내는건지 보류
-    // token 추가하기
-
-    // Create
     @PostMapping
-    public ResponseEntity<TaskToolBuilder> createTaskToolBuilder(@RequestBody TaskRequest taskRequest) {
-        TaskToolBuilder ttb = taskToolBuilderService.createTaskToolBuilder(taskRequest);
-        return ResponseEntity.ok(ttb);
-    }
-    // Get All
-    @GetMapping
-    public ResponseEntity<List<TaskToolBuilder>> getAllTaskToolBuilders() {
-        return ResponseEntity.ok(taskToolBuilderService.getAllTaskToolBuilder());
+    public ApiResponse<?> createTaskToolBuilder(@RequestBody TaskRequest taskRequest) {
+        return taskToolBuilderService.createTaskToolBuilder(taskRequest);
     }
 
-    // Get single
+    @GetMapping
+    public ApiResponse<?> getAllTaskToolBuilders() {
+        return taskToolBuilderService.getAllTaskToolBuilder();
+    }
+
     @GetMapping("/{id}")
-    public Optional<TaskToolBuilder> getSingleTaskToolBuilder(@PathVariable("id") int id) {
+    public ApiResponse<?> getSingleTaskToolBuilder(@PathVariable("id") int id) {
         return taskToolBuilderService.getTaskToolBuilderById(id);
     }
 
-    // Update single
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateTaskToolBuilder(@PathVariable("id") int id, @RequestBody TaskRequest taskRequest) {
-        taskToolBuilderService.updateTaskToolBuilder(id, taskRequest);
-        return ResponseEntity.ok().build();
+    public ApiResponse<?> updateTaskToolBuilder(@PathVariable("id") int id, @RequestBody TaskRequest taskRequest) {
+        return taskToolBuilderService.updateTaskToolBuilder(id, taskRequest);
     }
 
-    // Delete single
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteTaskToolBuilder(@PathVariable("id") int id) {
-        boolean success = taskToolBuilderService.deleteTaskToolBuilder(id);
-        if (success) {
-            return ResponseEntity.ok().build();
-        }else {
-            return ResponseEntity.notFound().build();
-        }
+    public ApiResponse<?> deleteTaskToolBuilder(@PathVariable("id") int id) {
+        return taskToolBuilderService.deleteTaskToolBuilder(id);
     }
 }
