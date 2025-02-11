041c3c8ba74f18a150c028e415603c1a04824787
KyungMin Mok <mok9603@naver.com>
2025-02-11 11:14:43 +0900

    Feat: workplace_move basic layout
    - 작업장 이동 기본 레이아웃
    - 스타일 조정 필요

--- frontend/src/app/router.jsx
+++ frontend/src/app/router.jsx
@@ -2,7 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import { WorkerLayout } from '@/shared/Layouts/WorkerLayout';
 import { ControlLayout } from '@/shared/Layouts/ControlLayout';
 import { LoginWorkerPage } from '@/pages/worker/LoginWorkerPage';
+import { WorkplaceMovePage } from '@/pages/worker/WorkplaceMovePage';
 import { LoginControlPage } from '@/pages/control/LoginControlPage';
+
 
 export const AppRouter = () => {
   return (
@@ -10,6 +12,7 @@ export const AppRouter = () => {
       {/* 작업자 영역 */}
       <Route path="/" element={<WorkerLayout />}>
         <Route path="login" element={<LoginWorkerPage />} />
+        <Route path="workplace_move" element={<WorkplaceMovePage />} />
       </Route>
 
       {/* 관제탑 영역 */}
