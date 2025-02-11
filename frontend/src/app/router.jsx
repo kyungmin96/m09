2dd5c1041667d051b74d998ed6fcb755b2f029c3
KyungMin Mok <mok9603@naver.com>
2025-02-11 11:23:19 +0900

    merge conflict

--- frontend/src/app/router.jsx
+++ frontend/src/app/router.jsx
@@ -2,17 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 
 import { WorkerLayout } from '@/shared/Layouts/WorkerLayout';
 import { ControlLayout } from '@/shared/Layouts/ControlLayout';
-<<<<<<< HEAD
 import { LoginWorkerPage } from '@/pages/worker/LoginWorkerPage';
 import { WorkplaceMovePage } from '@/pages/worker/WorkplaceMovePage';
 import { LoginControlPage } from '@/pages/control/LoginControlPage';
-=======
-
-import { LoginWorkerPage } from '@/pages/worker/LoginWorkerPage/index';
 import ToolCheckPage from '@/pages/worker/ToolCheckPage/index';
-import { LoginControlPage } from '@/pages/control/LoginControlPage/index';
->>>>>>> 11ce62d85d4fa4b39f975b87885b53ef82bdabc3
-
 
 export const AppRouter = () => {
   return (
@@ -20,11 +13,8 @@ export const AppRouter = () => {
       {/* 작업자 영역 */}
       <Route path="/" element={<WorkerLayout />}>
         <Route path="login" element={<LoginWorkerPage />} />
-<<<<<<< HEAD
         <Route path="workplace_move" element={<WorkplaceMovePage />} />
-=======
         <Route path="prepare-toolcheck" element={<ToolCheckPage />} />
->>>>>>> 11ce62d85d4fa4b39f975b87885b53ef82bdabc3
       </Route>
 
       {/* 관제탑 영역 */}
