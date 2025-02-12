722d5b7f2e42ad2d48d6b1f07d13421add428e8a
rokja97 <rokja97@gmail.com>
2025-02-12 09:46:45 +0900

    Fix: rearrange directory
    - 디렉토리 재배치

---
+++ embedded/orincar/m09_stream_uploader.py
@@ -0,0 +1,20 @@
+import cv2
+import socketio
+import os
+
+__address = os.environ["STREAM_SERVER_ADDRESS"] 
+sio_cli = socketio.Client()
+sio_cli.connect(__address)
+
+# cv 이미지 프레임을 지정된 웹소켓에 스트리밍
+def stream_cv_frame(frame):
+    bytecode = __bytecode_cv_frame(frame)
+    sio_cli.emit("stream", bytecode)
+
+# 바이트 코드로 변환
+def __bytecode_cv_frame(frame):
+    return cv2.imencode('.jpg', frame)[1].tobytes()
+
+@sio_cli.on("response")
+def response(data):
+    print(data)  # {'from': 'server'}
