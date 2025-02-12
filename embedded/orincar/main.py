c18bf9879c047eac206d8de85b74799bdca6d6a0
rokja97 <rokja97@gmail.com>
2025-02-12 09:44:59 +0900

    Fix: main script
    - 메인 스크립트 이름 수정
    - 카메라 모듈 대응하도록 수정

---
+++ embedded/orincar/main.py
@@ -0,0 +1,45 @@
+from m09_motor import *
+from m09_camera import *
+from matplotlib import use as matploblib_use
+from time import sleep
+import sys
+import os
+
+yolo_model = os.environ["M09_TRACK_MODEL"] 
+target_label = os.environ["M09_TARGET"]
+
+if __name__ == "__main__":
+    _headless = ("--headless" in sys.argv) or ("-hl" in sys.argv)
+    _manual = ("--manual" in sys.argv) or ("-m" in sys.argv)
+
+    if _headless:
+        matploblib_use("Agg")
+        print("[OrinCar] Headless Mode.")
+
+    motor_controller = motor_control(0)
+    cap = camera(640, 360)
+
+    if not _manual:
+        from m09_trace import *
+        object_tracer = trace(motor_controller, camera=cap, _headless=_headless, yolo_model=yolo_model, target_label=target_label)
+        object_tracer.start()
+        print("[OrinCar] Tracing Enabled!")
+    else:
+        from m09_ssh_control import *
+        ssh_controller = ssh_control(motor_controller)
+        ssh_controller.start()
+        print("[OrinCar] SSH Control Enabled!")
+
+    try:
+        while True:
+            sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+    
+    if not _manual:
+        object_tracer.stop()
+    else:
+        ssh_controller.stop()
+
+    motor_controller.terminate()
+    print("[OrinCar] Program stopped and motor stopped.")
