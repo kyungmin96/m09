27493e9d7dac5291c2f772f854862d5653c1a4f0
Doyeong Jung <sonatine789@naver.com>
2025-02-13 13:33:57 +0900

    Feat: add CORS Config settings
    - add CORS Config settings
    
    JIRA: S12P11A202-79

---
+++ backend/m09/src/main/java/ssafy/m09/config/WebConfig.java
@@ -0,0 +1,25 @@
+package ssafy.m09.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+//                .allowedOrigins(
+//                        "http://localhost:5173",
+//                        "https://ecdd-211-192-210-42.ngrok-free.app/"
+//                )  // 허용할 출처
+                .allowedOriginPatterns("*")  // 개발 환경에서는 모든 origin 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")  // JWT 관련 헤더 명시적 허용
+                .exposedHeaders("Authorization")  // 클라이언트가 읽을 수 있는 헤더 설정
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
