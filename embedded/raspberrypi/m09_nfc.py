9e06fbb823d3268d7e930164f0609110aa8ec264
rokja97 <rokja97@gmail.com>
2025-02-14 11:00:41 +0900

    Feat: server compatible
     - 서버와 연동 추가

--- embedded/raspberrypi/m09_nfc.py
+++ embedded/raspberrypi/m09_nfc.py
@@ -3,64 +3,114 @@ import board
 import busio
 from adafruit_pn532.spi import PN532_SPI
 from digitalio import DigitalInOut
-import signal
 from RPLCD.i2c import CharLCD
 import requests
 import json
 import RPi.GPIO as GPIO
+import os
+from threading import Thread
+import socketio
 
-# 요청을 보낼 URL
-url = "http://70.12.246.80:8080/api/rfid/login"
+# nfc 구조체
+class nfc:
+    def __init__(self):
+        self.base_url = os.environ["M09_BACKEND_ADDRESS"]
+        self.lcd = CharLCD('PCF8574', 0x27)
+        self._initiated = False
+        # PN532 SPI 설정
+        spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+        cs_pin = DigitalInOut(board.D6)
 
-# 요청 헤더 설정 (JSON 형식 명시)
-headers = {
-    "Content-Type": "application/json"
-}
-lcd = CharLCD('PCF8574', 0x27)
-lcd.write_string("Tag your ID Card")
+        # NFC 모듈 연결 확인
+        not_connected = True
+        self.pn532 = None
+        while not_connected:
+            try:
+                self.pn532 = PN532_SPI(spi, cs_pin, debug=False)
+                not_connected = False
+            except:
+                pass
 
-# 종료 시그널 핸들러
-do_scan = True
-def signal_handler(signum, frame):
-    global do_scan
-    do_scan = False
+        self.pn532.SAM_configuration()
+        
+        self._thread = None
+
+    def _run(self):
+        self._initiated = True
+        
+        # 요청을 보낼 URL
+        url = self.base_url + "/api/rfid/login"
+        # 요청 헤더 설정 (JSON 형식 명시)
+        headers = {
+            "Content-Type": "application/json"
+        }
+        
+        while self._initiated:
+            try:
+                self.lcd.write_string("Tag your ID Card")
+                uid = self.pn532.read_passive_target(timeout=0.5)
+                if uid:
+                    print("NFC 태그 감지!")
+                    # UID를 16진수 리스트로 변환
+                    arr = "".join([hex(i) for i in uid])
+                    print(arr)
+                    # JSON 형식으로 변환
+                    uid_json = json.dumps({"UID": arr}, indent=4)            
+                    # POST 요청 보내기 (json 매개변수 사용)
+                    response = requests.post(url, data=uid_json, headers=headers)
+                    print(response)
+                    self.lcd.clear()
+                    self.lcd.write_string('LOGIN SUCCESS!!!')
+                    sleep(1.5)
+                    self.lcd.clear()
+            except Exception as e:
+                print("NFC 읽기 오류:", e)
+
+    def start(self):
+        if self._thread and self._thread.is_alive():
+            self.stop()
+        self._thread = Thread(target=self._run)
+        self._thread.start()
+
+    def stop(self):
+        self._initiated = False
+        if self._thread and self._thread.is_alive():
+            self._thread.join()
+
+nfc_reader = nfc()
+__address = os.environ["M09_SERVER_ADDRESS"] 
+sio_cli = socketio.Client()
+sio_cli.connect(__address)
+print("Server Connected!")
+
+@sio_cli.event
+def nfc_start():
+    print("NFC Start!")
+    nfc_reader.start()
+
+@sio_cli.event
+def nfc_stop():
+    print("NFC Stop!")
+    nfc_reader.stop()
+
+@sio_cli.event
+def connect():
+    print("Connected to the server")
+
+# Event handler for when the server sends a message
+@sio_cli.event
+def message(data):
+    print(f"Received message from server: {data['data']}")
+
+# Event handler for when the client disconnects from the server
+@sio_cli.event
+def disconnect():
+    print("Disconnected from the server")
+
+try:
+    while True:
+        sleep(1)
+except KeyboardInterrupt:
+    print("Program end...")
     GPIO.cleanup()
-    exit()
-
-signal.signal(signal.SIGINT, signal_handler)
-
-# PN532 SPI 설정
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-cs_pin = DigitalInOut(board.D6)
-
-# NFC 모듈 연결 확인
-not_connected = True
-pn532 = None
-while not_connected:
-    try:
-        pn532 = PN532_SPI(spi, cs_pin, debug=False)
-        not_connected = False
-    except:
-        pass
-
-pn532.SAM_configuration()
-
-# NFC 태그 감지 루프
-while do_scan:
-    try:
-        uid = pn532.read_passive_target(timeout=0.5)
-        if uid:
-            print("NFC 태그 감지!")
-            # UID를 16진수 리스트로 변환
-            arr = "".join([hex(i) for i in uid])
-            print(arr)
-            # JSON 형식으로 변환
-            uid_json = json.dumps({"UID": arr}, indent=4)            
-            # POST 요청 보내기 (json 매개변수 사용)
-            response = requests.post(url, data=uid_json, headers=headers)
-            lcd.clear()
-            lcd.write_string('LOGIN SUCCESS!!!')
-            sleep(1.5)                                                            
-            
-    except Exception as e:
-        print("NFC 읽기 오류:", e)
+    pass
