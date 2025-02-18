cfb11464d2987c64ea9ce2274dde88bbef51fa9b
Doyeong Jung <sonatine789@naver.com>
2025-02-18 09:29:00 +0900

    Fix: SecurityConfig.java add import
    - add import
    
    JIRA: S12P11A202-80

--- backend/m09/src/main/java/ssafy/m09/config/SecurityConfig.java
+++ backend/m09/src/main/java/ssafy/m09/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ssafy.m09.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
